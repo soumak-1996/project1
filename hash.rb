6691625e26759ca7b60c67cf77692c00c7b6eedd
EC2 Default User <ec2-user@ip-172-31-39-170.us-east-2.compute.internal>
2018-03-06 06:33:56 +0000

    assignment1

--- hash.rb
+++ hash.rb
@@ -10,24 +10,40 @@ dictionary = {
     "Kochi" => "009" ,
     "Chennai" => "010" ,
 }
+
     def findCity(hash1 , city)
+        flag = 0
+        a = 0
         hash1.each do |key,value|
             if key == city
-                 puts "The value is #{value}"
+                flag = 1
+                a = value
+                 #puts "The value is #{value}"
                 break
             end
         end
+            if flag == 1
+                puts "#{a}"
+            else 
+                puts "City not found"
+            end
     end
     
-    
+    def Findcity(hash2)
+        hash2.each do |key,value|
+            puts "#{key}"
+        end
+    end
+
 =begin
     dictionary.each do |key,value|
-        puts "#{key} is the name of  #{value} city"
+        puts "#{key} is the
+        name of  #{value} city"
         end
 =end
         puts "Enter Y or N"
-        d = gets.chomp
-    while d != 'N'
+        d = gets.chomp.downcase
+    while d != 'n' 
         puts "Enter the city name"
         c = gets.chomp.to_s
 =begin        
@@ -40,10 +56,10 @@ dictionary = {
 =end
         findCity(dictionary , c)
          puts "Do you want to continue?"
-         d = gets.chomp
+         d = gets.chomp.downcase
     end
      
-         
+       Findcity(dictionary)  
          
          
          
